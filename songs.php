5cc86d083f0ddc90f8564207754ec9003e13e1ab
Florian_Mtds <florianmtddev@gmail.com>
2024-04-29 09:39:03 +0200

    ajout du tableau php songs

---
+++ songs.php
@@ -0,0 +1,35 @@
+<?php
+// Tableau multidimensionnel contenant les informations sur les chansons
+$songs = array(
+    array(
+        "title" => "Irridescent",
+        "artist" => "Linkin Park",
+        "time" => "4:56",
+        "albumImage" => "/assets/Icons/albumimg/linkingpark.png", // Chemin de l'image de l'album
+    ),
+    array(
+        "title" => "Thriller",
+        "artist" => "Michael Jackson",
+        "time" => "5:57",
+        "albumImage" => "/assets/Icons/albumimg/mj.png",
+    ),
+    array(
+        "title" => "Single Ladies",
+        "artist" => "Beyoncé",
+        "time" => "3:12",
+        "albumImage" => "/assets/Icons/albumimg/beyonce.png",
+    ),
+    array(
+        "title" => "Sweet Child O Mine",
+        "artist" => "Guns and Roses",
+        "time" => "5:57",
+        "albumImage" => "/assets/Icons/albumimg/gunsandroses.png",
+    ),
+    array(
+        "title" => "I'm Good (Blue)",
+        "artist" => "David Guetta",
+        "time" => "5:57",
+        "albumImage" => "/assets/Icons/albumimg/davidguetta.png",
+    ),
+);
+?>
